f8460365aee8e848844d51adb2fb5192ee03831c
Adrian Jules Sales <ajsales@berkeley.edu>
2021-04-28 00:38:16 -0700

    add in get

--- dist/app.js
+++ dist/app.js
@@ -12,6 +12,12 @@ var app = express();
 
 var server = require('http').createServer(app);
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://react-spotify-quiz.herokuapp.com"); // update to match the domain you will make the request from
+
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 var client = process.env.NODE_ENV === 'production' ? "https://react-spotify-quiz.herokuapp.com" : "http://localhost:3000";
 
 var io = require('socket.io')(server, {
