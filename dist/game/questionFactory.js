1514a928924c7c545cf4a5f1c7d94f1c2913a2a1
Adrian Jules Sales <ajsales@berkeley.edu>
2021-05-09 22:54:39 -0700

    Added comments to question factory

--- dist/game/questionFactory.js
+++ dist/game/questionFactory.js
@@ -9,8 +9,19 @@ var _getRandomInt = _interopRequireDefault(require("./helper/getRandomInt"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+/**
+ * Returns a function that generates questions
+ *
+ * @param {Object} top50 Extra Top 50 data to be added if not enough players
+ */
 function QuestionFactory(top50) {
+  /**
+   * Returns a random question.
+   *
+   * @param {Player[]} players Array of the players in the game
+   */
   var randomQuestion = function randomQuestion(players) {
+    // Restricts types of questions if there are less than 4 players
     var questions = [IdentifyFavoriteSong, IdentifyFavoriteArtist];
 
     if (players.length >= 4) {
@@ -23,6 +34,14 @@ function QuestionFactory(top50) {
 
     return question(players, option);
   };
+  /**
+   * Returns a question of the type:
+   * "What is one of Player 1's all-time Top 10 songs?"
+   *
+   * @param {Player[]} players Array of the players in the game
+   * @param {string} option The Top 10 lists to be used
+   */
+
 
   var IdentifyFavoriteSong = function IdentifyFavoriteSong(players, option) {
     var songs = _getRandomSongs(players, option);
@@ -46,6 +65,14 @@ function QuestionFactory(top50) {
       img: player.img
     };
   };
+  /**
+   * Returns a question of the type:
+   * "What is one of Player 1's all-time Top 10 artists?"
+   *
+   * @param {Player[]} players Array of the players in the game
+   * @param {string} option The Top 10 lists to be used
+   */
+
 
   var IdentifyFavoriteArtist = function IdentifyFavoriteArtist(players, option) {
     var artists = _getRandomArtists(players, option);
@@ -71,12 +98,20 @@ function QuestionFactory(top50) {
       img: player.img
     };
   };
+  /**
+   * Helper function to grab random songs from list of players
+   *
+   * @param {Player[]} players Array of the players in the game
+   * @param {string} option The Top 10 lists to be used
+   */
+
 
   var _getRandomSongs = function _getRandomSongs(players, option) {
     var songs = [];
 
     while (songs.length < 4) {
-      var song = void 0;
+      var song = void 0; // Grabs at least a player's favorite song for each player in the game
+      // Once it exceeds that, it'll grab a song from the extra Top 50 data
 
       if (songs.length < players.length) {
         var player = _getRandomArrEl(players);
@@ -84,7 +119,8 @@ function QuestionFactory(top50) {
         song = player.pickRandomSong(option);
       } else {
         song = _getRandomArrEl(top50.songs);
-      }
+      } // Doesn't add song if already in array
+
 
       if (!songs.map(function (s) {
         return s.toString;
@@ -95,12 +131,20 @@ function QuestionFactory(top50) {
 
     return _shuffle(songs);
   };
+  /**
+   * Helper function to grab random artists from list of players
+   *
+   * @param {Player[]} players Array of the players in the game
+   * @param {string} option The Top 10 lists to be used
+   */
+
 
   var _getRandomArtists = function _getRandomArtists(players, option) {
     var artists = [];
 
     while (artists.length < 4) {
-      var artist = void 0;
+      var artist = void 0; // Grabs at least a player's favorite artist for each player in the game
+      // Once it exceeds that, it'll grab an artist from the extra Top 50 data
 
       if (artists.length < players.length) {
         var player = _getRandomArrEl(players);
@@ -108,7 +152,8 @@ function QuestionFactory(top50) {
         artist = player.pickRandomArtist(option);
       } else {
         artist = _getRandomArrEl(top50.artists);
-      }
+      } // Doesn't add artist if already in array
+
 
       if (!artists.map(function (a) {
         return a.name;
@@ -119,10 +164,22 @@ function QuestionFactory(top50) {
 
     return _shuffle(artists);
   };
+  /**
+   * Helper function to grab random element from array
+   *
+   * @param {[]} arr Array to be used
+   */
+
 
   var _getRandomArrEl = function _getRandomArrEl(arr) {
     return arr[(0, _getRandomInt["default"])(arr.length)];
   };
+  /**
+   * Helper function to shuffle array
+   *
+   * @param {[]} array Array to be used
+   */
+
 
   var _shuffle = function _shuffle(array) {
     var currentIndex = array.length,
