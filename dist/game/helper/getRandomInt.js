caa5ea9c73c1d6aab1d4341fce32f0e14baf1c09
Adrian Jules Sales <ajsales@berkeley.edu>
2021-05-09 22:57:19 -0700

    Ran build

--- dist/game/helper/getRandomInt.js
+++ dist/game/helper/getRandomInt.js
@@ -5,6 +5,11 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = getRandomInt;
 
+/**
+ * Helper function to get random number within range (0, max - 1)
+ *
+ * @param {int} max Max number to include
+ */
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
--- dist/game/helper/getRandomString.js
+++ dist/game/helper/getRandomString.js
@@ -5,6 +5,11 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = getRandomString;
 
+/**
+ * Helper function to generate random string of a given length
+ *
+ * @param {int} length The length of the string to be returned
+ */
 function getRandomString(length) {
   var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   var result = '';
