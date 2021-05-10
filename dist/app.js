ab42bc4ff39779c30b7972d291a196a7b33a218d
Adrian Jules Sales <ajsales@berkeley.edu>
2021-05-09 22:32:43 -0700

    Added comments in player

--- dist/app.js
+++ dist/app.js
@@ -1,50 +1,86 @@
 "use strict";
 
-var _Game = _interopRequireDefault(require("./classes/Game"));
+var _Game = _interopRequireDefault(require("./game/Game"));
 
-var _Player = _interopRequireDefault(require("./classes/Player"));
+var _Player = _interopRequireDefault(require("./game/Player"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+/**
+ * Initalizes Socket.io server.
+ */
+// Sets up Express server
 var express = require('express');
 
 var app = express();
 
-var server = require('http').createServer(app);
+var server = require('http').createServer(app); // Headers needed for CORS
+
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://react-spotify-quiz.herokuapp.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
-var client = process.env.NODE_ENV === 'production' ? "https://react-spotify-quiz.herokuapp.com" : "http://localhost:3000";
+}); // Sets up origin (i.e. where Socket.io client is located)
+
+var client = process.env.NODE_ENV === 'production' ? "https://react-spotify-quiz.herokuapp.com" : "http://localhost:3000"; // Starts Socket.io server.
 
 var io = require('socket.io')(server, {
   cors: {
     origin: client,
     methods: ["GET", "POST"]
   }
-});
+}); // Helper classes for game/player
 
+
+/**
+ * Dictionary of all players in server.
+ *
+ * @typedef {Object.<string, Player>}
+ * @property {Player} playerId Instance of the player
+ */
 var players = {};
+/**
+ * Dictionary of all games in server.
+ *
+ * @typedef {Object.<string, Game>}
+ * @property {Game} gameId Instance of the game
+ */
+
 var games = {};
-var top50 = {};
+/**
+ * Extra Top 50 data to be placed in each game.
+ *
+ * @typedef {Object} 
+ * @property {Object[]} songs Songs in the Top 50 - USA playlist
+ * @property {Object[]} artists Artists of the above songs
+ */
+
+var top50 = {}; // Initializes the different server-side namespaces
+
 var callbackNamespace = io.of('/callback');
 var roomsNamespace = io.of('/rooms');
-var gameNamespaces = io.of(/^\/game\/\w{6}$/);
+var gameNamespaces = io.of(/^\/game\/\w{6}$/); // Matches all game URLs
+// Namespace to deal with callback page
+
 callbackNamespace.on('connection', function (socket) {
+  // Creates new Player instance and stores it into dictionary
+  // Also updates extra Top 50 data
   socket.on('newPlayer', function (playerData, extraData, callback) {
     var player = new _Player["default"](playerData);
     players[player.id] = player;
     top50 = extraData;
-    console.log('A new player connected:', player.name);
     callback();
+    console.log('A new player connected:', player.name);
   });
-});
+}); // Namespace to deal with rooms page
+
 roomsNamespace.on('connection', function (socket) {
+  // Sends available rooms to sockets
   var rooms = Object.keys(games);
   socket.emit('availableRooms', rooms);
-  console.log(socket.id, 'requesting rooms.');
+  console.log(socket.id, 'requesting rooms.'); // Creates new room (i.e. Game instance) and stores it into dictionary
+
   socket.on('newRoom', function (playerId, callback) {
     var host = players[playerId];
     var game = new _Game["default"](host, top50);
@@ -52,11 +88,15 @@ roomsNamespace.on('connection', function (socket) {
     callback(game.id);
     console.log('New game started:', game.id);
   });
-});
+}); // Namespaces to deal with game page
+// (Each individual game has their respective namespace)
+
 gameNamespaces.on('connection', function (socket) {
+  // Gets specific game
   var namespace = socket.nsp;
   var gameId = namespace.name.substring(6);
-  console.log('Someone connected to Game', gameId);
+  console.log('Someone connected to Game', gameId); // Sends question to game
+
   socket.on('questionRequest', function () {
     var game = games[gameId];
     socket.emit('newQuestion', game.question());
